bddc172d0756a15daa7bef807a21f96fbbb29ff2
Sana Sheikh <ss6231@nyu.edu>
2015-01-07 15:43:53 -0500

    Added support for images

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801190816) do
+ActiveRecord::Schema.define(version: 20150107202121) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "kind"
   end
 
 end
